c49ecb6f2b089cadf6f8bf3d37012e5f1e8d3b6b
Iman Kahbasi <ikahbasi@gmail.com>
2022-03-08 12:56:56 +0330

    Rewrite and update problem001.py in Python3

--- problem-001.py
+++ problem-001.py
@@ -1,7 +1,26 @@
+# Problem 1
 # Multiples of 3 and 5
 # Find the sum of all the multiples of 3 or 5 below 1000.
-a = 0
-for i in range(0, 1000):
-    if (i%3==0) or (i%5==0):
-        a += i
-print(a)
+
+###################################### solution 1
+
+sum_multiples = 0
+for num in range(0, 1000):
+    if (num%3==0) or (num%5==0):
+        sum_multiples += num
+print(sum_multiples)
+
+###################################### solution 2
+
+def range_generator(start, end):
+    num = start
+    while num < end:
+        yield num
+        num += 1
+
+
+sum_multiples = 0
+for num in range_generator(0, 1000):
+    if (num%3==0) or (num%5==0):
+        sum_multiples += num
+print(sum_multiples)
