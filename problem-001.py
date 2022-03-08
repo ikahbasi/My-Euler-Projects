1a32da0ba31081ed55900de386197ed273b49ddf
imankahbasi <ikahbasi@gmail.com>
2022-03-08 18:21:38 +0330

    The question format was changed

--- problem-001.py
+++ problem-001.py
@@ -1,17 +1,20 @@
-# Problem 1
-# Multiples of 3 and 5
-# Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+Multiples of 3 or 5
+Problem 1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 
 ###################################### solution 1
-
 sum_multiples = 0
 for num in range(0, 1000):
     if (num%3==0) or (num%5==0):
         sum_multiples += num
 print(sum_multiples)
 
-###################################### solution 2
 
+###################################### solution 2
 def range_generator(start, end):
     num = start
     while num < end:
