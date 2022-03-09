c35da5bbbf8c054c7440f11133c6e6dbb404932b
Iman Kahbasi <ikahbasi@gmail.com>
2022-03-09 10:04:18 +0330

    Convert from python2 to python3 and define functions.

--- problem-007.py
+++ problem-007.py
@@ -2,26 +2,28 @@
 #What is the 10 001st prime number?
 from math import sqrt
 
-def aval(n):
-	k=int(sqrt(n)+1)
-	avval=True
-	
-	for i in range(2,k):
-		if n%i==0:
-			avval=False
-			break
-	return avval
+def range_generator_forward(start, end, step=1):
+    num = int(start)
+    while num < end:
+        yield num
+        num += step
 
-count=0
-i=2
-while count<10001:
-	if aval(i):	
-		#print i ,'is aval'
-		count+=1
-		b=i
-		i+=1
-	else:
-		i+=1
 
-print 'count of aval number is',count
-print b
+def prime(num):
+    for j in range_generator_forward(start=2, end=sqrt(num)+1):
+        if not num % j:
+            return False
+    return True
+
+def nth_prime_number(th):
+	value = 0
+	counter = 0
+	while counter <= th:
+		if prime(value):
+			counter += 1
+			prime_value = value
+		value += 1
+	return prime_value
+
+p = nth_prime_number(10001)
+print(p)
