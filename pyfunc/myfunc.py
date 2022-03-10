38aaad475e5e8b3cf919ec4ac0151f68a1179e44
imankahbasi <ikahbasi@gmail.com>
2022-03-10 22:00:50 +0330

    Making repository of python functions

---
+++ pyfunc/myfunc.py
@@ -0,0 +1,30 @@
+from math import sqrt, ceil
+
+
+def range_generator_forward(start, end, step=1):
+    num = int(start)
+    while num < end:
+        yield num
+        num += step
+
+def isprime(num):
+    if num in [0, 1]:
+        prime = False
+    elif num == 2:
+        prime = True
+    else:
+        prime = True
+        for j in range_generator_forward(start=2, end=ceil(sqrt(num)+1)):
+            if not num % j:
+                prime = False
+    return prime
+
+def nth_prime_number(th):
+	value = 0
+	counter = 0
+	while counter <= th:
+		if isprime(value):
+			counter += 1
+			prime_value = value
+		value += 1
+	return prime_value
